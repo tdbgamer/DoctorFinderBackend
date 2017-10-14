0491e3847472b3aec2d00d8d9a818a26a6b4593c
man_nug <tbess@predikto.com>
2017-10-14 17:34:12 -0400

    setup heroku db

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
     pass
 
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI='FILL_IN_AFTER_MAKING_PROD_DB'
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI='postgresql://dgukmpjsoknhpi:c36296c6182e9d163e1a7d4db18cecbb77e5b3cece2a2977af2be9a088fdfa02@ec2-23-23-248-162.compute-1.amazonaws.com:5432/daksnb3kg25trr'
 
 class LocalConfig(Config):
     SQLALCHEMY_DATABASE_URI='sqlite:///test.db'
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config.LocalConfig')
+app.config.from_object('config.ProdConfig')
 db = SQLAlchemy(app)
 
 

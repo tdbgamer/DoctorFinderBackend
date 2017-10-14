d8f7ee74cbc3944eccf23e9b285aa10615e90432
man_nug <tbess@predikto.com>
2017-10-14 18:24:52 -0400

    * create manage.py * setup gitignore * create database migrations

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 app = Flask(__name__)
 app.config.from_object('config.ProdConfig')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
---
+++ manage.py
@@ -0,0 +1,6 @@
+from main import manager
+
+if __name__ == '__main__':
+    from models import *
+
+    manager.run()

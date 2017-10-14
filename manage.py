2442b1878881bda29b07d590178f31091faa8c6a
man_nug <tbess@predikto.com>
2017-10-14 18:39:52 -0400

    * convert fake data to csv * add new command to manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,19 @@
+from flask import current_app
+from flask_migrate import MigrateCommand
 from main import manager
+import csv
 
 if __name__ == '__main__':
     from models import *
+
+    manager.add_command('db', MigrateCommand)
+
+    @manager.command
+    def insert_data():
+        with open('fake_data.csv', 'r', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                # do stuff
+
 
     manager.run()

5e982cc14079d12977a9d06a8fd862b5e9beae0d
man_nug <tbess@predikto.com>
2017-10-14 20:26:48 -0400

    * remove CORS protection

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app, db)
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 @app.route('/')
 def main():
